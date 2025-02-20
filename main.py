d751daf230b3a9bbd49b1d2772ab3a38f9a79f65
Jorge Mora <jorgemorah671@gmail.com>
2025-02-20 17:45:06 -0600

    fix: :bug: How the board generate allowing to use n x n in binary

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
 import numpy as np
 
+def generate_n_numbers_of_n_bytes(n):
+  return [bytes(n) for _ in range(n)]
+
 class GA:
   def __init__(self, n, npop, feval, ngen, pmut, pcross, ptr_func_init=None):
     self.npop = npop
@@ -8,10 +11,10 @@ class GA:
     self.pmut = pmut
     self.pcross = pcross
     self.ptr_func_init = ptr_func_init
-    self.pop = np.zeros((self.npop, n * n))
-    self.pop_fit = np.zeros((self.npop, 1))
-    self.pop_select = np.zeros((self.npop, n * n))
-    self.offspring = np.zeros((self.npop, n * n))
+    self.pop = [generate_n_numbers_of_n_bytes(n) for _ in range(npop)]
+    self.pop_fit = np.zeros(npop)
+    self.pop_select = [generate_n_numbers_of_n_bytes(n) for _ in range(npop)]
+    self.offspring = [generate_n_numbers_of_n_bytes(n) for _ in range(npop)]
     
     # Initialize the population
   
