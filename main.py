ab90a4678b96d6c5126a94b655e2977b99343ec2
Jorge Mora <jorgemorah671@gmail.com>
2025-02-24 21:32:36 -0600

    feat: :sparkles: Implementation of prettyChessTablePrint function & defaultFuncInit function

--- main.py
+++ main.py
@@ -1,22 +1,69 @@
 import numpy as np
 
-def generate_n_numbers_of_n_bytes(n):
-  return [bytes(n) for _ in range(n)]
+def generateTableOfBytes(n):
+  return bytearray(n*n)
+
+def prettyChessTablePrint(chess_table, table_size, printBinary=False):
+  for row in range(0, len(chess_table), table_size):
+    for col in range(table_size):
+      pos = row + col
+      if chess_table[pos] == 1:
+        if printBinary:
+          print(' 1 ', end='')
+        else:
+          print(' Q ', end='')
+      elif chess_table[pos] == 0:
+        if printBinary:
+          print(' 0 ', end='')
+        else:
+          print(' . ', end='')
+    print()
+  print()
+    
+def defaultFuncInit(pop, table_size):
+  for individual in pop:
+    available_cols = list(range(table_size))
+    # Generate a queen in each row randomly
+    for row in range(0, len(individual), table_size):
+      # Avoid generating a queen in the same column
+      col = np.random.choice(available_cols)
+      available_cols.remove(col)
+      individual[row + col] = 1
+  return pop
+
+def defaultFEval(pop):
+  fitness = np.zeros(len(pop))
+
+  # Detect all the collisions using a mask
+  
 
 class GA:
-  def __init__(self, n, npop, feval, ngen, pmut, pcross, ptr_func_init=None):
-    self.npop = npop
-    self.feval = feval
-    self.ngen = ngen
-    self.pmut = pmut
-    self.pcross = pcross
-    self.ptr_func_init = ptr_func_init
-    self.pop = [generate_n_numbers_of_n_bytes(n) for _ in range(npop)]
-    self.pop_fit = np.zeros(npop)
-    self.pop_select = [generate_n_numbers_of_n_bytes(n) for _ in range(npop)]
-    self.offspring = [generate_n_numbers_of_n_bytes(n) for _ in range(npop)]
+  '''
+    table_size: Size of the chess table
+    nPop: Number of individuals in the population
+    fEval: Fitness function
+    nGen: Number of generations
+    pMut: Probability of mutation
+    pCross: Probability of crossover
+    ptrFuncInit: Function to initialize the population
+  '''
+  def __init__(self, table_size=8, nPop=100, fEval=None, nGen=100, pMut=0.1, pCross=0.5, ptrFuncInit=defaultFuncInit):
+    self.table_size = table_size
+    self.nPop = nPop
+    self.fEval = fEval
+    self.nGen = nGen
+    self.pMut = pMut
+    self.pCross = pCross
+    self.ptrFuncInit = ptrFuncInit
+
+    self.pop = [generateTableOfBytes(table_size) for _ in range(nPop)]
+    self.pop_fit = np.zeros(nPop)
+    self.pop_select = self.pop
+    self.offspring = self.pop
     
     # Initialize the population
-  
+    self.pop = self.ptrFuncInit(self.pop, table_size)
   def solve(self):
     pass
+
+ga = GA()
